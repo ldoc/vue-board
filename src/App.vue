3b172c4d7fc6a783b56ab5eda500060d2017cc92
Leandro <ext3-cti@ctisoluciones.com>
2018-06-28 10:16:48 +0200

    Initial config from vue-cli with some changes

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Board/>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Board
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hi from board
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
